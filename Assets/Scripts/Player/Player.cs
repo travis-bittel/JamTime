61b2c3c42655589e3aa44e64af0f8cbec3377e7b
travis-bittel <tbittel6@gatech.edu>
2021-07-09 18:18:13 -0400

    Minor Changes to Player Script, Make Player Orange

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -27,15 +27,19 @@ public class Player : MonoBehaviour
 {
 	public PlayerFacing direction;
 	public PlayerState state = PlayerState.IDLE;
-	public float SpeedScalar = 0.04f;
-	public Vector3 speed;
+	public float speedScalar;
+	public Vector3 velocity;
 
 	// Start is called before the first frame update
 	void Start()
 	{
+		if (speedScalar == 0)
+        {
+			Debug.LogWarning("Speed was set to 0, defaulting to 0.05");
+        }
 		direction = PlayerFacing.DOWN;
 		state = PlayerState.IDLE;
-		speed = new Vector3(0, 0, 0);
+		velocity = new Vector3(0, 0, 0);
 	}
 
 	// Update is called once per frame
@@ -43,14 +47,13 @@ public class Player : MonoBehaviour
 	{
 
 		// End of frame:
-		transform.position += speed * SpeedScalar;
+		transform.position += velocity * speedScalar;
 	}
 
 	// Happens onPress and Release.
 	public void OnMove(InputValue value)
 	{
-		Debug.Log("Henlo");
 		Vector2 moveDir = value.Get<Vector2>();
-		speed = new Vector3(moveDir.x, moveDir.y, 0);
+		velocity = new Vector3(moveDir.x, moveDir.y, 0);
 	}
 }
